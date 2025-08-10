d07c737f1f6b8035d372ac1bc8fe056b869fd046
maryamalihasanebrahim <maryamalihasanebrahim@gmail.com>
2025-08-10 18:12:27 +0300

    food card half done with axios calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Register from './pages/Register'
 import './App.css'
-
+import FoodCard from './components/FoodCard'
+import { Routes, Route } from 'react-router-dom'
 const App = () => {
-
   return (
     <>
-      <Register />
-      
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/foods/:id" element={<FoodCard />} />
+      </Routes>
     </>
   )
 }
