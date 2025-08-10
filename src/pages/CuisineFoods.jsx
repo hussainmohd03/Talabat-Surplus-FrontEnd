b4ef89b425705c2d3182f653ac73653c49c52f08
Hussain Ahmed <84622365+hussainmohd03@users.noreply.github.com>
2025-08-10 18:45:48 +0300

    edit home page style

--- src/pages/CuisineFoods.jsx
+++ src/pages/CuisineFoods.jsx
@@ -1,17 +1,23 @@
 import Search from '../components/Search'
 import { useSearchParams } from 'react-router-dom'
 import FoodCard from '../components/FoodCard'
+import { useEffect, useState } from 'react'
 
 const CuisineFoods = () => {
   const [searchParams] = useSearchParams()
   const cuisine = searchParams.get('cuisine')
+  const [cuisineFoods, setCuisineFoods] = useState(null)
+
+  useEffect(() => {
+
+  }, [cuisine])
 
   return (
     <>
       <Search />
 
       <section id="food-list">
-
+        
       </section>
     </>
   )
