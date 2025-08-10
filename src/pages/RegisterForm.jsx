6d42bd80eb6c2e31638414b697982c3b938c3ea4
Malak1805 <mjma200018@gmail.com>
2025-08-10 12:46:29 +0300

    completed the registeration forms for both customer and restaurant

--- src/pages/RegisterForm.jsx
+++ src/pages/RegisterForm.jsx
@@ -1,39 +1,83 @@
+import { useState } from "react"
+import { useNavigate } from 'react-router-dom'
 const RegisterForm = ({ role }) => {
+
+    let navigate = useNavigate()
+  
+  const initialState = {
+    firstName: '',
+    lastName: '',
+    Email: '',
+    Password: ''
+  }
+
+  const firstState = {
+    resName: '',
+    resPhone: '',
+    resAddress: '',
+    resEmail: '',
+    CR: ''
+  }
+
+  const [customerValues, setCustomerValues] = useState(initialState)
+    const [resValues, setResValues] = useState(firstState)
+
+
+  const handleChange = (e) => {
+    setCustomerValues({ ...customerValues, [e.target.name]: e.target.value })
+    setResValues({...resValues, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = async (e) => {
+    if(role === customerValues){
+    e.preventDefault()
+    await RegisterUser(customerValues)
+    setCustomerValues(initialState)
+    navigate('/LoginForm')
+   } else {
+      e.preventDefault()
+    await RegisterUser(resValues)
+    setResValues(firstState)
+    navigate('/LoginForm')
+    }
+  
   return (
     <div className="Register-Container">
       <h1>Sign Up as A {role}</h1>
 
       {role === 'customer' ? (
         <form>
-          <label>First Name</label>
-          <input type="text" />
-          <label>Last Name</label>
-          <input type="text" />
+          <label htmlFor="firstName">First Name</label>
+          <input type="text" placeholder="First Name" onChange={handleChange} value={customerValues.firstName} required/>
+          <label htmlFor="lastName">Last Name</label>
+          <input type="text" placeholder="Last Name" onChange={handleChange} value={customerValues.lastName} required/>
           <label>Email</label>
-          <input type="email" />
+          <input type="email" placeholder="Email" onChange={handleChange} value={customerValues.Email} required/>
           <label>Password</label>
-          <input type="password" />
+          <input type="password" placeholder="Password" onChange={handleChange} value={customerValues.Password} required/>
           <br />
           <button type="submit">Register</button>
         </form>
       ) : (
         <form>
-          <label>Restaurant Name</label>
+          <label htmlFor="resName">Restaurant Name</label>
           <input type="text" />
-          <label>Restaurant Telephone</label>
-          <input type="number" />
-          <label>Restaurant Address</label>
+          <label htmlFor="resPhone">Restaurant Telephone</label>
           <input type="text" />
-          <label>Restaurant Email</label>
+          <label htmlFor="resAddress">Restaurant Address</label>
+          <input type="text" />
+          <label htmlFor="resEmail">Restaurant Email</label>
           <input type="email" />
           <label>CR</label>
           <input type="number" />
           <br />
-          <button type="submit">Register</button>
+          <button type="submit">Continue</button>
+
         </form>
       )}
     </div>
   )
+}
 }
 
 export default RegisterForm
