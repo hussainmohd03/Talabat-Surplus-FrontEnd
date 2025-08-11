55bf8d8f62158c0f493fb19e23ccf67dd0d64bfe
Malak1805 <mjma200018@gmail.com>
2025-08-11 12:48:19 +0300

    adding basic styling for welcome page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 import RegisterForm from './RegisterForm'
 import LoginForm from './LoginForm'
 import { Link } from 'react-router-dom'
+import "../../public/styleSheets/Welcome.css"
 
 const Register = ({ role, choice, setRole, setChoice }) => {
   return (
-    <div>
+    <div className='welcome-container'>
+      <img src="../../img/logo.png" alt="" />
       <h1>Hey there!</h1>
       <p>Log in or sign up for a more personalized ordering experience </p>
       {!choice ? (
