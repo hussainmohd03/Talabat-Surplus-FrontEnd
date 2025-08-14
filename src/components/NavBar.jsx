fda3bc7caa64175952017d0924720e87ccb542bd
Malak1805 <mjma200018@gmail.com>
2025-08-14 09:03:22 +0300

    added styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     <>
       <nav>
         <div className="Links-container">
-        <Link to="/">Home</Link>
+        <Link to="/"> Home</Link>
         <Link to="/orders">Orders</Link>
         <Link to="/account">Account</Link>
         </div>
